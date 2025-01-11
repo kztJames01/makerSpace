7cd44fea2fea0fd42c80ba8f6dd7bc309bb06d72
Kaung Zaw Thant <kaung.thant@aiesec.net>
2025-01-10 22:02:51 -0600

    utilityfuntions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,66 +1,10 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 import qs from 'query-string'
-import { twMerge } from 'tailwind-merge'
-import { type ClassValue, clsx } from 'clsx'
 import {z} from 'zod'
-
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs))
 }
-
-export const formatDateTime = (dateString:Date)=>{
-
-    const dateTimeOptions : Intl.DateTimeFormatOptions = {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        hour12: true,
-        timeZoneName: 'short',
-    };
-
-    const dateDayOptions: Intl.DateTimeFormatOptions = {
-        weekday: 'short',
-        month: 'short',
-        day: '2-digit',
-        year: '2-digit',
-    };
-
-    const dateOptions : Intl.DateTimeFormatOptions = {
-        month: 'short',
-        day: 'numeric',
-        year: 'numeric',
-    };
-
-    const timeOptions : Intl.DateTimeFormatOptions = {
-        hour: 'numeric',
-        minute: 'numeric',
-        hour12: true,
-    };
-
-    const formatDateTime : string = new Date(dateString).toLocaleString('en-US', dateTimeOptions);
-    const formatDateDay : string = new Date(dateString).toLocaleString('en-US', dateDayOptions);
-    const formatDate : string = new Date(dateString).toLocaleString('en-US', dateOptions);
-    const formatTime : string = new Date(dateString).toLocaleString('en-US', timeOptions);
-
-    return {
-        dateTime: formatDateTime,
-        dateDay: formatDateDay,
-        date: formatDate,
-        time: formatTime
-    }
-}
-
-export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
-
-export const authFormSchema = (type:string)=> z.object({
-    firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3, 'First Name is required'),
-    lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3, 'Last name is required'),
-    email: z.string().email('Invalid email'),
-    password: z.string().min(8, 'Password is required'),
-    confirmPassword: type === 'sign-in' ? z.string().optional() : z.string().min(8, 'Password is required'),
-});
-
 export function UrlQuery({ params, key, value }: UrlQueryParams) {
     const query = qs.parse(params);
     query[key] = value;
@@ -72,10 +16,57 @@ export function UrlQuery({ params, key, value }: UrlQueryParams) {
         skipEmptyString: true,
     });
 }
+export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
+
+export const authFormSchema = (type:string)=> z.object({
+    firstName: type === 'sign-in'? z.string().optional(): z.string().min(2),
+    lastName: type === 'sign-in'? z.string().optional(): z.string().min(2),
+    email: z.string().email(),
+    password: z.string().min(6),
+    confirmPassword: type === 'sign-in'? z.string().optional(): z.string().min(6),
+})
 export function encryptId(id: string) {
     return btoa(id);
 }
+export const formatDateTime = (dateString: Date) => {
+    const dateTimeOptions: Intl.DateTimeFormatOptions = {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+    };
 
-export function decryptId(id: string) {
-    return atob(id);
-}
+    const dateDayOptions: Intl.DateTimeFormatOptions = {
+        weekday: 'short',
+        month: 'short',
+        day: '2-digit',
+        year: '2-digit',
+    };
+
+    const dateOptions: Intl.DateTimeFormatOptions = {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+    };
+
+    const timeOptions: Intl.DateTimeFormatOptions = {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+    };
+
+    const formatDateTime: string = new Date(dateString).toLocaleString('en-US', dateTimeOptions);
+    const formatDateDay: string = new Date(dateString).toLocaleString('en-US', dateDayOptions);
+    const formatDate: string = new Date(dateString).toLocaleString('en-US', dateOptions);
+    const formatTime: string = new Date(dateString).toLocaleString('en-US', timeOptions);
+
+    return {
+        dateTime: formatDateTime,
+        dateDay: formatDateDay,
+        date: formatDate,
+        time: formatTime,
+    };
+};
