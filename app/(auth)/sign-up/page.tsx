6adec4e19cf8c715d258132914545cedcc494518
Kaung Zaw Thant <kaung.thant@aiesec.net>
2025-01-10 23:18:36 -0600

    Sign Inand Sign Up page

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,3 +1,11 @@
-import React from 'react'
+import React from 'react';
+import AuthForm from '@/components/AuthForm';
 
-
+const SignUp = () => {
+    //  debugging const user = await getLoggedInUser();
+    return (
+        <section className='flex-center size-full max-sm:px-6'>
+            <AuthForm type='sign-up' />
+        </section>
+    )
+};
