f56b4313d5d86bc8f0e9dffa3aa9c4e1eeaa0fa4
Kaung Zaw Thant <kaung.thant@aiesec.net>
2025-02-01 23:41:07 -0600

    Added glassmorphism

--- makerspace/components/AuthForm.tsx
+++ makerspace/components/AuthForm.tsx
@@ -68,85 +68,87 @@ const AuthForm = ({ type }: { type: 'sign-in' | 'sign-up' }) => {
         }
     }
     return (
-        <section className='auth-form'>
-            <header className='flex flex-col gap-5 md:gap-8'>
-                <Link href='/' className='flex mb-12  cursor-pointer items-center gap-2'>
-                    <Image src="/logo/mobile-logo.png" width={34} height={34} alt="NxtGen logo" className="size=[24px] max-xl:size-14" />
-                    <h1 className=' font-bold text-24 font-robo text-gray-900 px-4 '>NxtGen</h1>
-                </Link>
-                <div className='flex flex-col gap-1 md:gap-3 '>
-                    <h1 className='text-24 lg:text-36 font-semibold text-gray-700'>
-                        {user ?
-                            'Link Account' : type === 'sign-in' ? 'Hop into the MakerSpace' : 'Start Your Jounrney'
-                        }
-                        <p className='text-16 font-normal text-gray-600'>
+        <section className='flex-center min-h-screen w-full'>
+            <div className="auth-form glassmorphism p-8 rounded-lg w-full max-w-md">
+                <header className='flex flex-col gap-5 md:gap-8'>
+                    <Link href='/' className='flex mb-12  cursor-pointer items-center gap-2'>
+                        <Image src="/logo/mobile-logo1.png" width={34} height={34} alt="NxtGen logo" className="size=[24px] max-xl:size-14" />
+                        <h1 className=' font-bold text-24 font-robo text-primary px-4 '>NxtGen</h1>
+                    </Link>
+                    <div className='flex flex-col gap-1 md:gap-3 '>
+                        <h1 className='text-24 lg:text-36 font-semibold text-primary'>
                             {user ?
-                                'Link your account to get started' : 'Please enter your details'}
-                        </p>
-                    </h1>
-                </div>
-            </header>
-            {user ?
-                (
-                    <div className='flex flex-col gap-4'>
-
-                    </div>
-                ) : (
-                    <>
-                        <Form {...form}>
-                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 font-geist-mono">
-                                {type === 'sign-up' && (
-                                    <>
-                                        <div className='flex gap-4 w-full'>
-                                            <CustomInput control={form.control} name="firstName" label="First Name" placeholder="Enter your first name" />
-                                            <CustomInput control={form.control} name="lastName" label="Last Name" placeholder="Enter your last name" />
-                                        </div>
-                                        <CustomInput control={form.control} name="email" label="Email" placeholder="Enter your email" />
-                                        <CustomInput control={form.control} name="password" label="Password" placeholder="Enter your password" />
-                                        <CustomInput control={form.control} name="confirmPassword" label="Confirm Password" placeholder="Confirm your password" />
-                                    </>
-                                )}
-                                {type === 'sign-in' && (
-                                    <>
-                                        <CustomInput
-                                            control={form.control}
-                                            name="email"
-                                            label="Email"
-                                            placeholder="Enter your email"
-                                        />
-                                        <CustomInput
-                                            control={form.control}
-                                            name="password"
-                                            label="Password"
-                                            placeholder="Enter your password"
-                                        />
-                                    </>
-                                )}
-
-
-                                <Button type="submit" disabled={loading} className="form-btn w-full">
-                                    {loading ?
-                                        <>
-                                            <Loader2 size={20} className='animate-spin mr-3' /> &nbsp;
-                                            Loading...
-                                        </>
-                                        : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
-                                </Button>
-
-                            </form>
-                        </Form>
-                        <footer className="flex justify-center gap-1">
-                            <p className='text-14 font-normal text-gray-600'>
-                                {type === 'sign-in' ? 'Don’t have an account?' : 'Already have an account?'}
+                                'Link Account' : type === 'sign-in' ? 'Hop into the MakerSpace' : 'Start Your Jounrney'
+                            }
+                            <p className='text-16 font-normal text-secondary'>
+                                {user ?
+                                    'Link your account to get started' : 'Please enter your details'}
                             </p>
-                            <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className='form-link'>
-                                {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
-                            </Link>
-                        </footer>
-                    </>
+                        </h1>
+                    </div>
+                </header>
+                {user ?
+                    (
+                        <div className='flex flex-col gap-4'>
 
-                )
-            }
+                        </div>
+                    ) : (
+                        <>
+                            <Form {...form}>
+                                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 font-geist-mono">
+                                    {type === 'sign-up' && (
+                                        <>
+                                            <div className='flex gap-4 w-full'>
+                                                <CustomInput control={form.control} name="firstName" label="First Name" placeholder="Enter your first name" />
+                                                <CustomInput control={form.control} name="lastName" label="Last Name" placeholder="Enter your last name" />
+                                            </div>
+                                            <CustomInput control={form.control} name="email" label="Email" placeholder="Enter your email" />
+                                            <CustomInput control={form.control} name="password" label="Password" placeholder="Enter your password" />
+                                            <CustomInput control={form.control} name="confirmPassword" label="Confirm Password" placeholder="Confirm your password" />
+                                        </>
+                                    )}
+                                    {type === 'sign-in' && (
+                                        <>
+                                            <CustomInput
+                                                control={form.control}
+                                                name="email"
+                                                label="Email"
+                                                placeholder="Enter your email"
+                                            />
+                                            <CustomInput
+                                                control={form.control}
+                                                name="password"
+                                                label="Password"
+                                                placeholder="Enter your password"
+                                            />
+                                        </>
+                                    )}
+
+
+                                    <Button type="submit" disabled={loading} className="form-btn w-full">
+                                        {loading ?
+                                            <>
+                                                <Loader2 size={20} className='animate-spin mr-3' /> &nbsp;
+                                                Loading...
+                                            </>
+                                            : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+                                    </Button>
+
+                                </form>
+                            </Form>
+                            <footer className="flex justify-center gap-1">
+                                <p className='text-14 font-normal text-secondary'>
+                                    {type === 'sign-in' ? 'Don’t have an account?' : 'Already have an account?'}
+                                </p>
+                                <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className='form-link'>
+                                    {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
+                                </Link>
+                            </footer>
+                        </>
+
+                    )
+                }
+            </div>
         </section>
     )
 }
