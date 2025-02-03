9b5019cb0aa925249e93e98887a7f23252082186
Kaung Zaw Thant <kaung.thant@aiesec.net>
2025-02-02 22:36:43 -0600

    Modified tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,27 +10,24 @@ export default {
   theme: {
   	extend: {
   		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
+  			background: {
+  				DEFAULT: 'var(--background)'
   			},
   			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
+  				DEFAULT: 'var(--secondary)'
+  			},
+  			foreground: {
+  				DEFAULT: 'var(--foreground)'
+  			},
+  			primary: {
+  				DEFAULT: 'var(--primary)'
+  			},
+  			primaryForeground: {
+  				DEFAULT: 'var(--primary-foreground)'
   			},
   			muted: {
   				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
+  				foreground: 'var(--muted-foreground)'
   			},
   			accent: {
   				DEFAULT: 'hsl(var(--accent))',
@@ -40,9 +37,9 @@ export default {
   				DEFAULT: 'hsl(var(--destructive))',
   				foreground: 'hsl(var(--destructive-foreground))'
   			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
+  			border: 'var(--border)',
+  			input: 'var(--input)',
+  			ring: 'var(--ring)',
   			chart: {
   				'1': 'hsl(var(--chart-1))',
   				'2': 'hsl(var(--chart-2))',
