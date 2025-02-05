ca3a2ead898723d6b2c312f43653deaaa4af80ed
Kaung Zaw Thant <kaung.thant@aiesec.net>
2025-02-04 22:41:56 -0600

    Added explore page

---
+++ app/explore/page.tsx
@@ -0,0 +1,13 @@
+// app/explore/page.tsx
+'use client';
+
+import { SidebarProvider } from '@/components/ui/sidebar';
+import FeedPage from '@/components/Feed';
+
+export default function ExplorePage() {
+    return (
+        <SidebarProvider>
+            <FeedPage />
+        </SidebarProvider>
+    );
+}
